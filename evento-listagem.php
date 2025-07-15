d5cf4dd6b6dea35bde24d031ac5ddaafccd4810f
joaomorenoo <joaovitorm.123@gmail.com>
2025-07-15 20:05:31 -0300

    estilização da tela

--- evento-listagem.php
+++ evento-listagem.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="PT-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Eventos</title>
 </head>
+<style>
+    * {
+        margin: 0px;
+        padding: 0px;
+        box-sizing: border-box;
+    }
+
+
+</style>
 <body>
     
 </body>

285069290cd37ae216ef82e2ddca8cfb7fcb9f0e
thomazandreywalicoski <thomazgithub@gmail.com>
2025-07-17 20:21:16 -0300

    atualizações login

--- database/www/index.php
+++ database/www/index.php
@@ -2,3 +2,15 @@
     include("../back/conecta.php");
     include("thomaz/menu.php");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Início</title>
+</head>
+<body>
+    <h1>TESTANDO........</h1>
+</body>
+</html>

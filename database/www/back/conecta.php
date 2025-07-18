fa7127141ae3b88f5c90dfae5f1d334fd7de9255
thomazandreywalicoski <thomazgithub@gmail.com>
2025-07-17 21:08:40 -0300

    atualizações login

--- database/www/back/conecta.php
+++ database/www/back/conecta.php
@@ -1,12 +1,13 @@
 <?php
-    $hostname = 'mysql';          // nome do serviço (container) no docker-compose
-    $username = 'evento';         // usuário criado no compose
-    $password = 'evento123';      // senha do usuário
-    $banco    = 'evento_db';      // nome do banco criado
+$hostname = 'mysql';          // nome do serviço MySQL no docker-compose
+$username = 'evento';         // usuário criado
+$password = 'evento123';      // senha
+$banco    = 'evento_db';      // banco de dados
+$porta    = 3306;             // porta interna do MySQL no container
 
-    $conecta = mysqli_connect($hostname, $username, $password, $banco);
+$conecta = mysqli_connect($hostname, $username, $password, $banco, $porta);
 
-    if (!$conecta) {
-        die("A conexão falhou: " . mysqli_connect_error());
-    }
+if (!$conecta) {
+    die("A conexão falhou: " . mysqli_connect_error());
+}
 ?>
